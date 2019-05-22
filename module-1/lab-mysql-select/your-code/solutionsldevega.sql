1290d50e39d647ddbe064177c629d8297fdfb28d
lisethdevegab <lisethc.vega@gmail.com>
2019-05-22 19:09:45 +0200

    ACT [lab-mysql-select] [dptmad] [Liseth DE Vega]

--- module-1/lab-mysql-select/your-code/solutionsldevega.sql
+++ module-1/lab-mysql-select/your-code/solutionsldevega.sql
@@ -1,13 +1,14 @@
+
  -- challengue1
- 
-SELECT
+
+ SELECT
  authors.au_id AS 'Author ID',
  authors.au_lname AS 'LAST NAME',
  authors.au_fname AS 'FIRST NAME',
  titles.title AS 'Titles',
  publishers.pub_name AS 'PUBLISHER'
- 
- FROM
+
+  FROM
  authors
  INNER JOIN
  titleauthor
@@ -21,21 +22,21 @@ SELECT
  publishers
  ON 
  titles.pub_id = publishers.pub_id
- 
- ORDER BY
+
+  ORDER BY
 authors.au_id 
 
- 
-  -- challengue2
- 
- SELECT
+
+   -- challengue2
+
+  SELECT
  authors.au_id AS 'Author ID',
  authors.au_lname AS 'LAST NAME',
  authors.au_fname AS 'FIRST NAME',
  publishers.pub_name AS 'PUBLISHER',
  COUNT(titles.title) AS 'TITLES COUNT'
- 
- FROM
+
+  FROM
  authors
  INNER JOIN
  titleauthor
@@ -50,20 +51,20 @@ authors.au_id
  ON 
  titles.pub_id = publishers.pub_id
 
- GROUP BY
+  GROUP BY
 authors.au_id, publishers.pub_name
 
-ORDER BY
+ ORDER BY
 count(titles.title) DESC, authors.au_id desc, publishers.pub_id desc
 
--- challengue 3
+ -- challengue 3
 SELECT
  authors.au_id AS 'Author ID',
  authors.au_lname AS 'LAST NAME',
  authors.au_fname AS 'FIRST NAME',
-sum(sales.qty) AS 'Total'
- 
- FROM
+ sum(sales.qty) AS 'Total'
+
+  FROM
  authors
  INNER JOIN
  titleauthor
@@ -77,16 +78,16 @@ sum(sales.qty) AS 'Total'
  sales
  ON 
  sales.title_id = titles.title_id
- 
- GROUP BY
+
+  GROUP BY
  authors.au_id
- 
- ORDER BY
+
+  ORDER BY
 sum(sales.qty) DESC
 
-LIMIT 3
+ LIMIT 3
 
--- challengue 4
+ -- challengue 4
 SELECT  
  authors.au_id AS 'Author ID',
  authors.au_lname AS 'LAST NAME',
@@ -114,15 +115,14 @@ authors.au_id
  order by
 sum(sales.qty) desc
 
-
--- bonus challengue, me falta algo, lo sé, seguiré intentando el tema de calcular el PROFIT
+ -- bonus challengue, me falta algo, lo sé, seguiré intentando el tema de calcular el PROFIT
 SELECT  
  authors.au_id AS 'Author ID',
  authors.au_lname AS 'LAST NAME',
  authors.au_fname AS 'FIRST NAME',
  sum(titles.advance)+sum(titles.royalty) AS 'PROFIT'
- 
- FROM
+
+  FROM
  authors
  INNER JOIN
  titleauthor
@@ -132,13 +132,12 @@ SELECT
  titles
  ON
  titleauthor.title_id = titles.title_id
- 
- 
- GROUP BY
+
+
+  GROUP BY
  authors.au_id
- 
- ORDER BY
+
+  ORDER BY
 sum(titles.advance)+sum(titles.royalty) DESC
 
-LIMIT 3
-
+ LIMIT 3
